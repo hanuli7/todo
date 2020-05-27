c4b2368be054d444895124617e702592034a4505
hanuli7 <65943965+hanuli7@users.noreply.github.com>
2020-05-27 17:51:28 +0900

    publish react todo app

---
+++ precache-manifest.16c049294d0ae0dfdb0bae6217e2dcb4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad90d83eef14e94a7def8db482642bbf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8a30a3292b84f0d8cbd7",
+    "url": "/static/css/main.083460f9.chunk.css"
+  },
+  {
+    "revision": "3f00501c6c2eadca48a5",
+    "url": "/static/js/2.8c0f9739.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8c0f9739.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a30a3292b84f0d8cbd7",
+    "url": "/static/js/main.d254e69a.chunk.js"
+  },
+  {
+    "revision": "0467cf6e03846fdf98aa",
+    "url": "/static/js/runtime-main.360ed79e.js"
+  }
+]);
